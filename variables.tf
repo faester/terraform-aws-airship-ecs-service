7e2d8dbc6cf13f94ab0805ae3291ad00a6e37164
Mads Hvelplund <mads.hvelplund@jppol.dk>
2019-05-22 11:07:34 +0200

    Changed service defaults
    * Name is now a required attribute
    * Redirect to HTTPS is optional
    * Default platform is now FARGATE

--- variables.tf
+++ variables.tf
@@ -3,19 +3,22 @@ variable "create" {
   default     = true
 }
 
+variable "name" {
+	description = "The name of the project, must be unique and match"
+}
+
 variable "shared_settings" {
   description = <<EOF
     The are the settings that are typically shared between all services. If a value is present it the "settings" variable, it overrides the value here (if any). All values are REQUIRED.
     Sample:
 
     shared_service_settings = {
-      environment_name      = "..." # The environment name, eg 'kitdev'
-      lb_route53_zone_id    = "..." # The zone to add a service subdomain to
-      mgmt_account          = "..." # Id of the management account containing ECR images
-      region                = "..." # Region of the ECS Cluster
+      environment_name   # The environment name, eg 'kitdev'
+      lb_route53_zone_id # The zone to add a service subdomain to
+      mgmt_account       # Id of the management account containing ECR images
+      region             # Region of the ECS Cluster
     }
   EOF
-
   type = "map"
 }
 
@@ -25,23 +28,23 @@ variable "settings" {
     Sample:
 
     settings = {
-      name                      = "..." # The name of the project, must be unique (REQUIRED)
-      bootstrap_container_image = "..." # The docker image location. Set to USE_DEFAULT or give a docker image path with version label      
-      container_cpu             = "..." # Defines the needed cpu for the container
-      container_memory          = "..." # Defines the hard memory limit of the container
-      container_port            = "..." # Container port
-      initial_capacity          = "..." # The desired amount of tasks for a service, when autoscaling is used desired_capacity is only used initially
-      kms_keys                  = "..." # Comma separated list of KMS keys that the service can access
-      ssm_paths                 = "..." # Comma separated list of SSM keys that the service can access
-      s3_ro_paths               = "..." # Comma separated list of S3 Bucket/Prefixes that the service can access
-      s3_rw_paths               = "..." # Comma separated list of S3 Bucket/Prefixes that the service can access
-      load_balancing_type       = "..." # The load balancer type ("application"/"none"). Defaults to "application"
-      lb_health_uri             = "..." # Load balancer health check URL
-      lb_unhealthy_threshold    = "..." # The number of consecutive successful health checks required before considering an healthy target unhealthy
-      lb_healthy_threshold      = "..." # The number of consecutive successful health checks required before considering an unhealthy target healthy
-      max_capacity              = "..." # When autoscaling is activated, it sets the maximum of tasks to be available for this service
-      min_capacity              = "..." # When autoscaling is activated, it sets the minimum of tasks to be available for this service
-      platform                  = "..." # Either FARGATE or EC2 (uppercase)
+      bootstrap_container_image # The docker image location. Set to USE_DEFAULT or give a docker image path with version label      
+      container_cpu             # Defines the needed cpu for the container (default is 256)
+      container_memory          # Defines the hard memory limit of the container (default is 512)
+      container_port            # Container port (default is 8080)
+      initial_capacity          # The desired amount of tasks for a service, when autoscaling is used desired_capacity is only used initially (default is 1)
+      kms_keys                  # Comma separated list of KMS keys that the service can access (default is "")
+      lb_health_uri             # Load balancer health check URL  (default is "/health")
+      lb_healthy_threshold      # The number of consecutive successful health checks required before considering an unhealthy target healthy (default is 3)
+      lb_redirect_http_to_https # Redirect all HTTP requests to HTTPS (default is true)
+      lb_unhealthy_threshold    # The number of consecutive successful health checks required before considering an healthy target unhealthy (default is 3)
+      load_balancing_type       # The load balancer type ("application"/"none"). (Default is "application")
+      max_capacity              # When autoscaling is activated, it sets the maximum of tasks to be available for this service (default is 2)
+      min_capacity              # When autoscaling is activated, it sets the minimum of tasks to be available for this service (default is 1)
+      platform                  # Either FARGATE or EC2 (default is "FARGATE")
+      s3_ro_paths               # Comma separated list of S3 Bucket/Prefixes that the service can access (default is "")
+      s3_rw_paths               # Comma separated list of S3 Bucket/Prefixes that the service can access (default is "")
+      ssm_paths                 # Comma separated list of SSM keys that the service can access (default is "")
     }
   EOF
 
