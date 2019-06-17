67b9e28678a9843831b6e0b253fd34b41bc5e65e
Morten Fæster <morten.faester@jppol.dk>
2019-06-17 12:34:11 +0200

    Added airship outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,31 @@ output "settings" {
   description = "The final settings passed to Airship. Useful for debugging."
   value       = "${local.combined_settings}"
 }
+
+output "aws_ecs_task_definition_arn" {
+  value = "${module.service.aws_ecs_task_definition_arn}"
+}
+
+output "aws_ecs_task_definition_family" {
+  value = "${module.service.aws_ecs_task_definition_family}"
+}
+
+output "ecs_taskrole_arn" {
+  value = "${module.service.ecs_taskrole_arn}"
+}
+
+output "ecs_taskrole_name" {
+  value = "${module.service.ecs_taskrole_name}"
+}
+
+output "has_changed" {
+  value = "${module.service.has_changed}"
+}
+
+output "lb_target_group_arn" {
+  value = "${module.service.lb_target_group_arn}"
+}
+
+output "task_execution_role_arn" {
+  value = "${module.service.task_execution_role_arn}"
+}
